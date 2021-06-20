712925e7c8ffbe6863ab86eb0953e57ccf105518
tortsBsus <meganraj251@gmail.com>
2021-06-20 17:21:06 +0530

    20-06-21 Substitution encryption done

--- SubstitutionImplementation.java
+++ SubstitutionImplementation.java
@@ -3,33 +3,61 @@ import java.util.Scanner;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
 class Substitution
 {
    File Code;
    File Input;
    File Output;
+   HashMap<Character,Character> Co = new HashMap<>();
    Substitution(File C, File I, File O)
    {
             Input = I;
             Code = C;
             Output = O;
    }
-   File Substitute()
+   void Hash_it(int a, int b)
    {
       try
       {
-         Scanner Reads =  new Scanner(Input);
+         Scanner CReads = new Scanner(Code);
+                     
+         while(CReads.hasNextLine())
+         {
+            String A = CReads.nextLine();            
+            Co.put(A.charAt(a),A.charAt(b));
+         }
+         CReads.close();
+      }
+      catch(FileNotFoundException X)
+      {
+         System.out.println("File doesn't exist");
+         X.printStackTrace();
+      }   
+      finally
+      {
+        System.out.println("Hashed");       
+      }
+   }
+   void Substitute(int F, int S)
+   {
+      //Creates HashMap
+      Hash_it(F,S);
+      try
+      {
+         Scanner IReads =  new Scanner(Input);
          FileWriter Writes = new FileWriter(Output);
          
-         while(Reads.hasNextLine())
+         while(IReads.hasNextLine())
          {
-            String A = Reads.nextLine();
-            
-            Writes.append(Line);
-            Writes.append("\n");
-
+            String A = IReads.nextLine();
+            for(int a=0;a<A.length();a++)
+            {
+              Writes.append(Co.get(A.charAt(a)));
+            }
+          
          }
-         Reads.close();
+         IReads.close();
          Writes.close();
       }
       catch(FileNotFoundException X)
@@ -38,15 +66,11 @@ class Substitution
          X.printStackTrace();
       }   
       catch(IOException p)
-      {
-         
+      {        
          System.out.println("FileWriter error");
          p.printStackTrace();
       }
-   }
-   File UnSubstitute()
-   {
-   
+            
    }
    
 }
@@ -55,7 +79,22 @@ class SubstitutionImplementation
 {
     public static void main(String args[])
    {
-      Substitution A = new Substitution(new File("Code.txt"),new File("Input.txt"),new File("Output.txt"));      
+       
+      
+      Substitution A = new Substitution(new File("Code.txt"),new File("Input.txt"),new File("Output.txt"));
+      Scanner S = new Scanner(System.in);
+      System.out.println("Do you want to \n1.Encrypt\n2.Decrypt");
+      int ch = S.nextInt();
+      int a=0,b=3;
+      switch(ch)
+      {
+         case 1:break;
+         case 2:a=3;b=0;break;
+         default:System.out.println("Wrong input. Encrypting by default");
+         
+      }      
+      A.Substitute(a,b);
+            
    }
 
 }
